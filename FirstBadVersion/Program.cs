2b9d878326ce3f77fbdc904303925632a288ea37
rgv3300 <rgv3300@gmail.com>
2021-09-17 17:59:23 -0400

    First Bad version problem

---
+++ FirstBadVersion/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace FirstBadVersion
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int n = 2126753390;
+            Solution.FirstBadVersion(n);
+        }
+        public class VersionControl
+        {
+            public static bool IsBadVersion(int n)
+            {
+                if (n == 1702766719) { return true; }
+                else { return false; }
+            }
+        }
+        public class Solution : VersionControl
+        {
+            public static int FirstBadVersion(int n)
+            {
+                int left = 1;
+                int right = n;
+                while (left < right)
+                {
+                    int mid = left + (right - left) / 2;
+                    if (IsBadVersion(mid))
+                    {
+                        right = mid;
+                    }
+                    else
+                    {
+                        left = mid + 1;
+                    }
+                }
+                return left;
+            }
+        }
+    }
+}
