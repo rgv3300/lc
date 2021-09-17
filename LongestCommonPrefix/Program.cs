e0f27b49ffc60088b4eed0917a1c3574e00b2aea
rgv3300 <rgv3300@gmail.com>
2021-09-17 16:42:48 -0400

    longest common prefix problem

---
+++ LongestCommonPrefix/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LongestCommonPrefix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strs = { "Flower", "Flex", "Flamboyant", "flight" };
+            Solution.LCP(strs);
+        }
+        public class Solution
+        {
+            public static string LCP(string[] strs)
+            {
+                if (strs.Length == 0) return "";
+                string prefix = strs[0];
+                for (int i = 1; i < strs.Length; i++)
+                {
+                    while (strs[i].IndexOf(prefix) != 0)
+                    {
+                        prefix = prefix.Substring(0, prefix.Length - 1);
+                        if (String.IsNullOrEmpty(prefix)) return "";
+                    }
+                }
+                return prefix;
+            }
+        }
+    }
+}
