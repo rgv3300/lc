935e0f8dbbc242585a560a2479abfafc0ffe7014
rgv3300 <rgv3300@gmail.com>
2021-09-19 16:47:05 -0400

    Sorted Array square

---
+++ SortedArraySquare/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SortedArraySquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { -10, -4, 3, 5, 6 };
+
+            Solution.SortedSquares(nums);
+        }
+        public class Solution
+        {
+            public static int[] SortedSquares(int[] nums)
+            {
+                for (int i = 0; i < nums.Length; i++)
+                    nums[i] *= nums[i];
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    int min_idx = i;
+                    for (int j = i + 1; j < nums.Length; j++)
+                        if (nums[j] < nums[min_idx])
+                            min_idx = j;
+                    int temp = nums[min_idx];
+                    nums[min_idx] = nums[i];
+                    nums[i] = temp;
+                }
+                return nums;
+            }
+        }
+    }
+}
