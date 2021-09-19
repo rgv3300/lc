bf1fc62c6370dd24ecc3ad0a05be003087fa27a6
rgv3300 <rgv3300@gmail.com>
2021-09-19 15:56:56 -0400

    Search insert position

---
+++ SearchInsertPos/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SearchInsertPos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 3, 4, 5, 6, 9, 66, 67 };
+            int target = 65;
+            Solution.SearchInsert(nums, target);
+        }
+
+        public class Solution
+        {
+            public static int SearchInsert(int[] nums, int target)
+            {
+                int left = 0;
+                int right = nums.Length;
+                while (left < right)
+                {
+                    int pivot = left + (right - left) / 2;
+                    if (nums[pivot] == target) return pivot;
+                    if (nums[pivot] < target)
+                    {
+                        left = pivot + 1;
+                    }
+                    else
+                    {
+                        right = pivot;
+                    }
+                }
+                if (left == right)
+                {
+                    return left;
+                }
+                return left - 1;
+            }
+        }
+    }
+}
