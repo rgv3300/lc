bc5d6909198a9743580e81ee3c2a14948c4add91
rgv3300 <rgv3300@gmail.com>
2021-09-20 16:58:04 -0400

    Rotate array: : ;

---
+++ RotateArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+namespace RotateArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 17 };
+            int target = 5;
+            Solution.Rotate(nums, target);
+        }
+
+        public class Solution
+        {
+            public static void Rotate(int[] nums, int k)
+            {
+                int[] m = new int[k];
+                int t = 0;
+                for (int i = nums.Length - k; i < nums.Length; i++)
+                {
+                    m[t] = nums[i];
+                    t++;
+                }
+                for (int j = 0; j < m.Length; j++)
+                {
+                    nums = nums.Where(val => val != m[j]).ToArray();
+                }
+                nums = m.Concat(nums).ToArray();
+                for (int i = 0; i < nums.Length; i++)
+                    Console.WriteLine("" + nums[i]);
+            }
+        }
+    }
+}
