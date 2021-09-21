559864319a8eafdfa4f357d71642d51b0be4df8b
rgv3300 <rgv3300@gmail.com>
2021-09-21 15:22:37 -0400

    TwoSumSorted not solved:

---
+++ TwoSumSorted/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TwoSumSorted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 4, 9, 56, 90 };
+            int target = 8;
+            Solution.TwoSum(numbers, target);
+        }
+        public class Solution
+        {
+            public static int[] TwoSum(int[] numbers, int target)
+            {
+                int[] result = new int[2];
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    int l = i;
+                    int r = numbers.Length - 1;
+                    int complement = target - numbers[i];
+                    while (r >= l)
+                    {
+                        int pivot = l + (r - l) / 2;
+
+
+                        if (numbers[pivot] == complement)
+                        {
+                            result[0] = i;
+                            result[1] = pivot;
+                            return result;
+                        }
+                        else if (numbers[pivot] < complement)
+                        {
+                            l = pivot + 1;
+                        }
+                        else
+                        {
+                            r = pivot - 1;
+                        }
+
+                    }
+                }
+                return result;
+            }
+        }
+    }
+}
