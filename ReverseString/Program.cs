f1eba5e2e5c526845382ce10770dc190074d3c95
rgv3300 <rgv3300@gmail.com>
2021-09-21 15:52:29 -0400

    Reverse String using two pointer

---
+++ ReverseString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] s = { 'h', 'e', 'l', 'l', 'o' };
+            Solution.ReverseString(s);
+        }
+        public class Solution
+        {
+            public static void ReverseString(char[] s)
+            {
+                for (int i = 0; i < s.Length; i++)
+                {
+                    char temp = s[i];
+                    s[i] = s[s.Length - i - 1];
+                    s[s.Length - i - 1] = temp;
+                }
+            }
+        }
+    }
+}
